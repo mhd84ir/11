7a88ae8b82ea41361328a389098a854de9e20b5e
unknown <mhd84.ir@gmail.com>
2022-03-11 12:38:44 +0330

    switch

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿System.Console.WriteLine("adad bede");
+int number=Convert.ToInt32(Console.ReadLine()
+);
+switch (number)
+{
+    case 1:System.Console.WriteLine("January");
+        break;
+    case 2:System.Console.WriteLine("February");
+        break;
+    case 3:System.Console.WriteLine("March");
+        break;                
+    case 4:System.Console.WriteLine("April");
+        break;        
+    case 5:System.Console.WriteLine("May");
+        break;
+    case 6:System.Console.WriteLine("June");
+        break;
+    case 7:System.Console.WriteLine("July");
+        break;
+    case 8:System.Console.WriteLine("August");
+        break;
+    case 9:System.Console.WriteLine("September");
+        break;
+    case 10:System.Console.WriteLine("October");
+        break;
+    case 11:System.Console.WriteLine("November");
+        break;
+    case 12:System.Console.WriteLine("December");
+        break;
+    default:System.Console.WriteLine("eshtebah");
+        break;
+}
